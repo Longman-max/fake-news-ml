b0d0d21f0a61fff4fed6e826aeb257078c011d71
Longman-max <147675147+Longman-max@users.noreply.github.com>
2025-07-25 21:45:39 +0100

    trained model added backend logic gitignore script

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,9 @@
-ECHO is on.
+import joblib
+
+model = joblib.load("models/model.pkl")
+vectorizer = joblib.load("models/vectorizer.pkl")
+
+def predict_news(text):
+    vect_text = vectorizer.transform([text])
+    prediction = model.predict(vect_text)
+    return prediction[0]
--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,28 @@
-ECHO is on.
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+import os
+
+df = pd.read_csv("../data/fake_or_real_news.csv")
+
+X = df['text']
+y = df['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+model = LogisticRegression()
+model.fit(X_train_tfidf, y_train)
+
+y_pred = model.predict(X_test_tfidf)
+print(classification_report(y_test, y_pred))
+
+os.makedirs("models", exist_ok=True)
+joblib.dump(model, "models/model.pkl")
+joblib.dump(vectorizer, "models/vectorizer.pkl")
